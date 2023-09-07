5a2802b7535b8e045744f9ecbb7987dc1478e477
민동후 <mdh9701@naver.com>
2023-09-07 17:16:43 +0900

    [#30] feat: main 페이지 구성

--- ssafi-fe/src/main.tsx
+++ ssafi-fe/src/main.tsx
@@ -3,15 +3,21 @@ import React from 'react';
 import styled from 'styled-components';
 import mainImage from './assets/images/main-image.png';
 import buttonArrow from './assets/images/button-arrow.png';
+import longLogo from './assets/logos/logo-long.png';
+import aiTradeImage from './assets/images/ai-trade.svg';
+import investPortfolio from './assets/images/invest-portfolio.svg';
+import stockNews from './assets/images/stock-news.svg';
 
 const MainContainer = styled.div`
   display: flex;
+  flex-direction: column;
+  align-items: center;
   min-width: 1270px;
-  justify-content: center;
 `;
 
 const MainBanner = styled.div`
-  width: 1270px;
+  display: flex;
+  width: 1210px;
   margin-top: 75px;
   padding: 0px 30px;
   position: relative
@@ -69,6 +75,72 @@ const BannerImg = styled.img.attrs({
   margin-right: 30px;
 `;
 
+const MainService = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 1210px;
+  padding: 160px 30px;
+`;
+
+const LongLogo = styled.img.attrs({
+  src: `${longLogo}`,
+})`
+  width:  670px;
+`;
+
+const ServiceDiv = styled.div`
+  display: flex;
+  margin-top: 50px;
+  width: 1000px;
+  justify-content: space-between;
+`;
+
+const ServiceBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 280px;
+  height: 400px;
+  background: var(--light-gradation-color);
+`;
+
+const ServiceTitle = styled.p`
+  color: var(--dark-color);
+  font-size: 24px;
+  font-weight: 500;
+  margin: 20px;
+`;
+
+const ServiceContent = styled.p`
+  color: var(--gray-color);
+  font-size: 20px;
+  font-weight: 400;
+  margin: 0px;
+`;
+
+const AiTradeImg = styled.img.attrs({
+  src: `${aiTradeImage}`,
+})`
+  width: 160px;
+  margin-bottom: 10px;
+`;
+
+const InvestPortfolioImg = styled.img.attrs({
+  src: `${investPortfolio}`,
+})`
+  width: 160px;
+  margin-bottom: 10px;
+`;
+
+const StockNewsImg = styled.img.attrs({
+  src: `${stockNews}`,
+})`
+  width: 160px;
+  margin-bottom: 10px;
+`;
+
 export default function Main() {
   return (
     <MainContainer>
@@ -97,6 +169,38 @@ export default function Main() {
         </div>
         <BannerImg />
       </MainBanner>
+      <MainService>
+        <LongLogo />
+        <ServiceDiv>
+          <ServiceBox>
+            <AiTradeImg />
+            <ServiceTitle>
+              AI 트레이딩
+            </ServiceTitle>
+            <ServiceContent>
+              AI를 활용해 일하는 시간에<br/>도 주식 투자를 해보세요
+            </ServiceContent>
+          </ServiceBox>
+          <ServiceBox>
+            <InvestPortfolioImg />
+            <ServiceTitle>
+              투자 포트폴리오
+            </ServiceTitle>
+            <ServiceContent>
+              투자성향을 분석해 포트폴<br/>리오를 추천받으세요
+            </ServiceContent>
+          </ServiceBox>
+          <ServiceBox>
+            <StockNewsImg />
+            <ServiceTitle>
+              증권 뉴스
+            </ServiceTitle>
+            <ServiceContent>
+              관심종목과 관련된 뉴스<br/>를 확인하세요
+            </ServiceContent>
+          </ServiceBox>
+        </ServiceDiv>
+      </MainService>
     </MainContainer>
   );
 }
