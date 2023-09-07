b30318a086fbbbd5ac25029b755a42b7c919ed88
민동후 <mdh9701@naver.com>
2023-09-07 14:16:55 +0900

    [#27]feat: navbar css 파일 삭제, styled-component 추가

--- ssafi-fe/src/components/Header/index.tsx
+++ ssafi-fe/src/components/Header/index.tsx
@@ -1,43 +1,97 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
-import './styles.css';
+import { useNavigate, useLocation } from 'react-router-dom';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import styled from 'styled-components';
 import darkLogo from '../../assets/logos/logo-dark.png';
 
 export default function Header() {
+  // 기능 코드 파트
+  const navigate = useNavigate();
   const location = useLocation();
 
   const toMain = () => {
-    window.location.href = '/';
+    navigate('/');
   };
 
-  const toPortfolio = () => {
-    window.location.href = '/survey';
+  const toSurvey = () => {
+    navigate('/survey');
   };
+
+  // styled-component 파트
+  const NavContainer = styled.div`
+    display: flex;
+    min-width: 1270px;
+    height: 60px;
+    border-bottom: 1px solid var(--black-color);
+    justify-content: center;
+    color: var(--white-color);
+    background-color: var(--dark-color);
+  `;
+
+  const Navbar = styled.div`
+    display: flex;
+    justify-content: space-between;
+    width: 1270px;
+    align-items: center;
+    padding: 0px 30px;
+  `;
+
+  const NavbarLeft = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  `;
+
+  const SiteLogo = styled.img.attrs({
+    src: `${darkLogo}`,
+  })`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    cursor: pointer;
+    margin-right: 30px;
+    height: 28px;
+  `;
+
+  const SiteMenu = styled.div`
+  font-size: 24px;
+  margin-left: 30px;
+  cursor: pointer;
+  color: ${(props) => (props.active ? 'var(--point-color)' : 'var(--white-color)')};
+`;
+
+  const NavbarRight = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  `;
+
+  const LoginButton = styled.div`
+    font-size: 18px;
+    cursor: pointer;
+  `;
 
   return (
-    <div className='nav-area'>
-      <div className='navbar'>
-        <div className='navbar-left'>
-          <div className='site-logo' onClick={toMain}>
-            <img src={darkLogo} height={33} ></img>
-          </div>
-          <div className='site-menu'>
-            <p>AI 트레이딩</p>
-          </div>
-          <div className='site-menu' onClick={toPortfolio}>
-            <p style={ (location.pathname === '/survey')
-              ? { color: 'var(--point-color)' } : { color: 'var(--white-color)' }}>포트폴리오</p>
-          </div>
-          <div className='site-menu'>
-            <p>뉴스</p>
-          </div>
-        </div>
-        <div className='navbar-right'>
-          <div className='login'>
-            <p>로그인</p>
-          </div>
-        </div>
-      </div>
-    </div>
+    <NavContainer>
+      <Navbar>
+        <NavbarLeft>
+          <SiteLogo onClick={toMain} />
+          <SiteMenu active={location.pathname === '/trade'}>
+            AI 트레이딩
+          </SiteMenu>
+          <SiteMenu active={location.pathname === '/survey'} onClick={toSurvey}>
+            포트폴리오
+          </SiteMenu>
+          <SiteMenu active={location.pathname === '/news'}>
+            뉴스
+          </SiteMenu>
+        </NavbarLeft>
+        <NavbarRight>
+          <LoginButton>
+            로그인
+          </LoginButton>
+        </NavbarRight>
+      </Navbar>
+    </NavContainer>
   );
 }
