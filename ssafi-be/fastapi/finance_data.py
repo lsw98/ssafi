b34de7b49e2bbd28ef652dc88835ca15f3988485
lsw98 <sangwoo9808@gmail.com>
2023-09-11 17:52:32 +0900

    feat : KOSPI200 종목코드, requirements 작성

---
+++ ssafi-be/fastapi/finance_data.py
@@ -0,0 +1,21 @@
+import FinanceDataReader as fdr
+import pandas as pd
+import mplfinance as mpf
+
+kospi_200 = pd.read_csv('kospi200.csv', encoding='EUC-KR')
+kospi = pd.DataFrame(kospi_200)
+kospi_dict = {}
+
+# 코스피 200 종목코드, 종목명 딕셔너리 생성
+for index, row in kospi.iterrows():
+    code = str(row['종목코드'])
+    length = len(code)
+    if length < 6:
+         for _ in range (6-length):
+             code = '0' + code
+    kospi_dict[code] = row['종목명']
+print(kospi_dict)
+
+for code in kospi_dict:
+    df = fdr.DataReader(code, '2023-08-01', '2023-09-11')
+    fdr.chart.plot(df)
