0e805ac07da6c8af27693cca816ac0e86f802802
윤지현 <gus991121@naver.com>
2023-09-11 12:35:56 +0900

    feat: footer 하단 고정

--- ssafi-fe/src/App.tsx
+++ ssafi-fe/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Router from './router/Router';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
       <Router></Router>
+      <Footer></Footer>
     </>
   );
 }
--- ssafi-fe/src/router/Router.tsx
+++ ssafi-fe/src/router/Router.tsx
@@ -2,23 +2,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from '../components/Header';
 import Main from '../main';
 import Portfolio from '../portfolio';
-import Footer from '../components/Footer';
+// import Footer from '../components/Footer';
 
 export default function Router() {
-  function MainWithFooter() {
-    return (
-      <>
-        <Main />
-        <Footer />
-      </>
-    );
-  }
-
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<MainWithFooter />} />
+        <Route path="/" element={<Main />} />
         <Route path="/survey" element={<Portfolio />} />
       </Routes>
     </BrowserRouter>
