7312aaea09fcd3ecbf2c02d878180e783f0315e5
윤지현 <gus991121@naver.com>
2023-09-11 12:33:16 +0900

    chore: eslint 에러 해결

--- ssafi-fe/src/router/Router.tsx
+++ ssafi-fe/src/router/Router.tsx
@@ -1,11 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-// eslint-disable-next-line import/no-unresolved, import/extensions
 import Header from '../components/Header';
-// eslint-disable-next-line import/extensions, import/no-unresolved
 import Main from '../main';
-// eslint-disable-next-line import/extensions, import/no-unresolved
 import Portfolio from '../portfolio';
-// eslint-disable-next-line import/no-unresolved, import/extensions
 import Footer from '../components/Footer';
 
 export default function Router() {
