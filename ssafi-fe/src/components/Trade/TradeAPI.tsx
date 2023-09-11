d657dd3d3846df2a4128e4386c80e0b404a2f2fe
gus991121 <gus991121@naver.com>
2023-09-11 21:28:57 +0900

    [#99] feat: api key 입력 화면 구성

--- ssafi-fe/src/components/Trade/TradeAPI.tsx
+++ ssafi-fe/src/components/Trade/TradeAPI.tsx
@@ -6,14 +6,17 @@ const APIContainer = styled.div`
   justify-content: center;
   align-items: center;
   width: 100%;
-  height: 477px;
+  height: 500px;
   position: relative;
 `;
 
 const DarkContainer = styled.div`
-  position: absolute;
-  width: 1000px;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  width: 1100px;
   height: 200px;
+  position: absolute;
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
@@ -23,12 +26,12 @@ const DarkContainer = styled.div`
 
 const WhiteContainer = styled.div`
   display: flex;
-  align-content: space-around;
-  position: absolute;
+  justify-content: center;
+  align-items: center;
   width: 470px;
   height: 300px;
   position: absolute;
-  left: 30%;
+  left: 25%;
   top: 50%;
   transform: translate(-40%, -50%);
   background-color: var(--white-color);
@@ -36,30 +39,70 @@ const WhiteContainer = styled.div`
 `;
 
 const InnerContainer = styled.div`
-  width: 400px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  width: 85%;
+  height: 80%;
+`;
 
-  &.dark {
-    padding-left: 550px;
+const Text = styled.div`
+  font-weight: 400;
+  font-size: 16px;
+  color: var(--white-color);
+  margin: 15px 0;
+
+  &.title {
+    font-size: 20px;
   }
 `;
 
-const Title = styled.div`
-  font-weight: 400;
+const InputKey = styled.div`
+  font-weight: 500;
   font-size: 24px;
-  color: var(--white-color);
+  color: var(--point-color);
+  margin: 20px 0;
+
+  &.button {
+    background-color: var(--point-color);
+    color: var(--white-color);
+    align-self: flex-end;
+    padding: 4px 16px;
+    cursor: pointer;
+  }
+`;
+
+const InputBox = styled.input`
+  width: 100%;
+  border: none;
+  border-bottom: 1px solid var(--gray-color);
+  outline: none;
+  font-size: 18px;
+  margin-top: 10px;
+  
+  &::placeholder {
+    color: var(--gray-color);
+  }
 `;
 
 export default function TradeAPI() {
   return (
     <APIContainer>
       <DarkContainer>
-        <InnerContainer className='dark'>
-          <Title>API Key가 없으신가요?</Title>
-        </InnerContainer>
+        <div style={{ width: '530px' }}>
+          <InnerContainer>
+            <Text className='title'>API Key가 없으신가요?</Text>
+            <Text>SSAFI에서는 ~~님의 api key를 통해 ~~하고 있으며, <br/>
+              api key 인증한 회원에 한해 ai 트레이딩 서비스를 제공하고 있습니다.</Text>
+            <Text>API Key 발급받으러 가기 &gt;</Text>
+          </InnerContainer>
+        </div>
       </DarkContainer>
       <WhiteContainer>
         <InnerContainer>
-          <Title style={{ color: 'var(--point-color)' }}>API Key를 입력해주세요.</Title>
+          <InputKey>API Key를 입력해주세요.</InputKey>
+          <InputBox placeholder="API Key를 입력하세요"/>
+          <InputKey className='button'>입력하기</InputKey>
         </InnerContainer>
       </WhiteContainer>
     </ APIContainer>
