c3dac337831300de60e5d11b62dadfc4f4c45e3b
gofoxfox <gofoxfox@naver.com>
2023-09-19 17:27:51 +0900

    feat: 한국투자증권 접근토큰 발급 API 구현

--- ssafi-be/ssafi/src/main/java/com/run/ssafi/stock/feign/KISAuthApi.java
+++ ssafi-be/ssafi/src/main/java/com/run/ssafi/stock/feign/KISAuthApi.java
@@ -1,18 +1,15 @@
 package com.run.ssafi.stock.feign;
 
 import com.run.ssafi.social.config.FeignConfiguration;
+import com.run.ssafi.stock.dto.KISAccessTokenRequestDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(value = "KISAuth", url="https://openapivts.koreainvestment.com:29443", configuration = {
         FeignConfiguration.class})
 public interface KISAuthApi {
     @PostMapping("/oauth2/tokenP")
-    ResponseEntity<String> getAccessToken(
-            @RequestParam("grant_type") String grantType,
-            @RequestParam("appKey") String appKey,
-            @RequestParam("appsecret") String appSecret
-    );
+    ResponseEntity<String> getAccessToken(@RequestBody KISAccessTokenRequestDto kisAccessTokenRequestDto);
 }
