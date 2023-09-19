c8b31ded1d1922a074e28c3bf5c858bf75ddabe2
민동후 <mdh9701@naver.com>
2023-09-19 11:06:04 +0900

    [#155] feat: api 가이드용 틀 구성

--- ssafi-fe/src/components/Trade/ApiGuide.tsx
+++ ssafi-fe/src/components/Trade/ApiGuide.tsx
@@ -25,12 +25,56 @@ const ModalContainer = styled.div`
   transition: height 0.3s;
 `;
 
-const ApiGuide = ({ closeModal }: GuideProps) => (
-  <ModalBackground onClick={closeModal}>
-    <ModalContainer>
-      api key 받아오는 방법 설명
-    </ModalContainer>
-  </ModalBackground>
-);
+const ButtonBox = styled.div`
+display: flex;
+justify-content: end;
+align-items: center;
+`;
+
+const questionList: Array<string> = ['0', '1', '2', '3', '4', '5', '6', '7'];
+const allQuestions = questionList.length - 1;
+
+const ApiGuide = ({ closeModal }: GuideProps) => {
+  const [questionNum, setQuestionNum] = React.useState(0);
+
+  const nextQuestionNum = () => {
+    if (questionNum < allQuestions) {
+      setQuestionNum(questionNum + 1);
+    }
+  };
+
+  const previousQuestionNum = () => {
+    if (questionNum > 0) {
+      setQuestionNum(questionNum - 1);
+    }
+  };
+
+  const stopPropagation = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+  };
+
+  return (
+    <ModalBackground onClick={closeModal}>
+      <ModalContainer onClick={stopPropagation}>
+        api key 받아오는 방법 설명
+        <div>
+          <h3>{questionList[questionNum]}</h3>
+          {questionNum !== allQuestions
+            ? <div>
+                { questionNum === 0
+                  ? <ButtonBox><button onClick={nextQuestionNum}>다음</button></ButtonBox>
+                  : <ButtonBox>
+                    <button onClick={previousQuestionNum}>이전</button>
+                    <button onClick={nextQuestionNum}>다음</button>
+                  </ButtonBox>
+                }
+              </div>
+            : <ButtonBox><button onClick={closeModal}>창 닫기</button></ButtonBox>
+          }
+        </div>
+      </ModalContainer>
+    </ModalBackground>
+  );
+};
 
 export default ApiGuide;
