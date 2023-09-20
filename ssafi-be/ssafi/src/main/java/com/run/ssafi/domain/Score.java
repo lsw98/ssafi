8ba2436a3c8aa95b8faf8aaeb44fdf95fc9c8a25
gofoxfox <gofoxfox@naver.com>
2023-09-20 14:48:02 +0900

    style: 코드 포맷팅 및 불필요 코드 제거

--- ssafi-be/ssafi/src/main/java/com/run/ssafi/domain/Score.java
+++ ssafi-be/ssafi/src/main/java/com/run/ssafi/domain/Score.java
@@ -4,8 +4,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
--- ssafi-be/ssafi/src/main/java/com/run/ssafi/member/repository/ScoreRepository.java
+++ ssafi-be/ssafi/src/main/java/com/run/ssafi/member/repository/ScoreRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface ScoreRepository extends JpaRepository<Score, Long>, MemberCustomRepository {
+public interface ScoreRepository extends JpaRepository<Score, Long> {
 
     Optional<Score> findById(Long id);
 
