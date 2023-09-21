6ec4cc0936ea1425c6c46b1669608f3cff36ec52
윤지현 <gus991121@naver.com>
2023-09-21 13:27:34 +0900

    [#168] feat: 투자비율 자동채움, 직접 입력 기능 구현

--- ssafi-fe/src/components/Trade/TradeInput.tsx
+++ ssafi-fe/src/components/Trade/TradeInput.tsx
@@ -2,10 +2,6 @@ import React, { useState } from 'react';
 import styled, { keyframes } from 'styled-components';
 import { ReactComponent as Doubts } from '../../assets/images/doubts-button.svg';
 
-interface StyleProps {
-  show: boolean;
-}
-
 interface TradeInputProps {
   isTrade: boolean;
   setIsTrade: React.Dispatch<React.SetStateAction<boolean>>;
@@ -45,7 +41,7 @@ const Label = styled.label`
   color: var(--white-color);
 `;
 
-const SelectOptions = styled.ul<StyleProps>`
+const SelectOptions = styled.ul<{show: boolean}>`
   position: absolute;
   list-style: none;
   top: 32px;
@@ -65,7 +61,6 @@ const Option = styled.li`
   font-size: 18px;
   padding: 6px 0;
   transition: background-color 0.2s ease-in;
-
   &:hover {
     font-size: 19px;
     background-color: var(--sub-color);
@@ -94,15 +89,6 @@ const DoubtsButton = styled(Doubts)`
   cursor: pointer;
 `;
 
-const fadeIn = keyframes`
-  from {
-    opacity: 0;
-  }
-  to {
-    opacity: 1;
-  }
-`;
-
 const Tooltip = styled.div`
   width: 200px;
   position: absolute;
@@ -114,7 +100,10 @@ const Tooltip = styled.div`
   padding: 6px 12PX;
   border-radius: 6px;
   display: 'block';
-  animation: ${fadeIn} 0.3s ease-in-out;
+  animation: ${keyframes`
+    from { opacity: 0; }
+    to { opacity: 1; }
+  `} 0.3s ease-in-out;
 
   &::after {
     content: '';
@@ -176,16 +165,22 @@ const StopBtn = styled.div<{stop: boolean}>`
   background: ${(props) => (props.stop ? '' : 'var(--point-color)')};
 `;
 
+const createInputHandler = (setter:React.Dispatch<React.SetStateAction<number>>) => (
+  event:React.ChangeEvent<HTMLInputElement>,
+) => {
+  const newValue = Number.parseInt(event.target.value, 10);
+  setter(newValue);
+};
+
 export default function TradeInput({ isTrade, setIsTrade }: TradeInputProps) {
   const [currentValue, setCurrentValue] = useState('투자 성향');
   const [showOptions, setShowOptions] = useState(false);
   const [showTooltip, setShowTooltip] = useState(false);
-  // 순서대로 안전, 중립, 위험 종목의 비율
-  const [safeValue, setSafeValue] = useState(0);
-  const [middleValue, setMiddleValue] = useState(0);
-  const [dangerValue, setDangerValue] = useState(0);
-  const [investmentAmount, setInvestmentAmount] = useState(0);
-  const [targetAmount, setTargetAmount] = useState(0);
+  const [safetyRatio, setSafetyRatio] = useState(0);
+  const [neutralRatio, setNeutralRatio] = useState(0);
+  const [riskRatio, setRiskRatio] = useState(0);
+  const [aiBudget, setAiBudget] = useState(0);
+  const [aiGoal, setAiGoal] = useState(0);
 
   const handleOnChangeSelectValue = (event: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
     const { innerText } = event.currentTarget;
@@ -193,14 +188,10 @@ export default function TradeInput({ isTrade, setIsTrade }: TradeInputProps) {
 
     const selectedOption = options.find((option) => option.type === innerText);
     if (selectedOption) {
-      setSafeValue(selectedOption.rates[0]);
-      setMiddleValue(selectedOption.rates[1]);
-      setDangerValue(selectedOption.rates[2]);
+      setSafetyRatio(selectedOption.rates[0]);
+      setNeutralRatio(selectedOption.rates[1]);
+      setRiskRatio(selectedOption.rates[2]);
     }
-  };
-
-  const handleDoubtsButtonHover = (isHovered: boolean) => {
-    setShowTooltip(isHovered);
   };
 
   const options = [
@@ -286,15 +277,12 @@ export default function TradeInput({ isTrade, setIsTrade }: TradeInputProps) {
         <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
           <Notice>금융 MBTI 결과를 바탕으로 종목별 주식 투자 비율을 추천해드려요.</Notice>
           <DoubtsButtonWrapper
-            onMouseEnter={() => handleDoubtsButtonHover(true)}
-            onMouseLeave={() => handleDoubtsButtonHover(false)}
+            onMouseEnter={() => setShowTooltip(true)}
+            onMouseLeave={() => setShowTooltip(false)}
           >
             <DoubtsButton/>
             {showTooltip && (
-              <Tooltip>
-                아직 나의 금융 MBTI를 모르시나요?
-                <br /> 금융 MBTI 알아보러 가기
-              </Tooltip>
+              <Tooltip>아직 나의 금융 MBTI를 모르시나요?<br /> 금융 MBTI 알아보러 가기</Tooltip>
             )}
           </DoubtsButtonWrapper>
         </div>
@@ -304,19 +292,19 @@ export default function TradeInput({ isTrade, setIsTrade }: TradeInputProps) {
           <RateOfType>
             <Label>안전</Label>
             <Input
-              type='number' min='0' max='100'value={safeValue}
+              type='number' min='0' max='100'value={safetyRatio} onChange={createInputHandler(setSafetyRatio)}
             />
           </RateOfType>
           <RateOfType>
             <Label>중립</Label>
             <Input
-              type='number' min='0' max='100' value={middleValue}
+              type='number' min='0' max='100' value={neutralRatio} onChange={createInputHandler(setNeutralRatio)}
             />
           </RateOfType>
           <RateOfType>
             <Label>위험</Label>
             <Input
-              type='number' min='0' max='100' value={dangerValue}
+              type='number' min='0' max='100' value={riskRatio} onChange={createInputHandler(setRiskRatio)}
             />
           </RateOfType>
         </RateContainer>
@@ -325,13 +313,13 @@ export default function TradeInput({ isTrade, setIsTrade }: TradeInputProps) {
       <RateContainer>
         <Label>투자 금액</Label>
         <Input
-          type='number' min='0' className='ammount' value={investmentAmount}
+          type='number' min='0' className='ammount' value={aiBudget} onChange={createInputHandler(setAiBudget)}
         />
       </RateContainer>
       <RateContainer>
         <Label>목표 금액</Label>
         <Input
-          type='number' min='0' className='ammount' value={targetAmount}
+          type='number' min='0' className='ammount' value={aiGoal} onChange={createInputHandler(setAiGoal)}
         />
       </RateContainer>
       <RightBox>
