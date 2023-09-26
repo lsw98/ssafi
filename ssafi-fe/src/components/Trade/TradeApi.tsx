91cc1ab5c6aecbee71195689d9e5a44f4b7b5b4e
윤지현 <gus991121@naver.com>
2023-09-26 17:36:10 +0900

    [#232] feat: 투자 기본 정보 입력 레이아웃 재구성

--- ssafi-fe/src/components/Trade/TradeApi.tsx
+++ ssafi-fe/src/components/Trade/TradeApi.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import { ReactComponent as Doubts } from '../../assets/images/doubts-button.svg';
@@ -23,7 +23,7 @@ const Container = styled.div<ContainerProps>`
   align-items: center;
   justify-content: ${(props) => (props.dark ? 'flex-end' : 'center')};
   width: ${(props) => (props.dark ? '1100px' : '470px')};
-  height: ${(props) => (props.dark ? '210px' : '300px')};
+  height: ${(props) => (props.dark ? '300px' : '420px')};
   position: absolute;
   top: 50%;
   left: ${(props) => (props.dark ? '50%' : '25%')};
@@ -37,7 +37,7 @@ const InnerContainer = styled.div`
   flex-direction: column;
   justify-content: space-around;
   width: 85%;
-  height: 80%;
+  height: 70%;
 `;
 
 const Text = styled.div`
@@ -46,41 +46,54 @@ const Text = styled.div`
   font-weight: 400;
   font-size: 20px;
   color: var(--white-color);
-  margin: 12px 0;
-
+  margin: 16px 0;
   &.small {
-    font-size: 14px;
-    line-height: 22px;
+    font-size: 16px;
+    font-weight: 300;
+    line-height: 26px;
   }
-
   &.button {
     font-size: 16px;
     cursor: pointer;
   }
-  `;
+`;
 
-const InputKey = styled.div<{disabled: boolean}>`
-  font-weight: 500;
-  font-size: 24px;
-  color: var(--point-color);
-  margin: 20px 0;
+const InputBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+`;
+
+const InputName = styled.div<{visibled?: boolean}>`
+  font-size: 20px;
+  &.name {
+    width: 31%;
+    text-align: end;
+  }
+
+  &.header {
+    font-weight: 500;
+    font-size: 22px;
+    color: var(--point-color);
+  }
 
   &.button {
-    background-color: ${(props) => (props.disabled ? 'var(--light-gray-color)' : 'var(--point-color)')};
+    background-color: ${(props) => (props.visibled ? 'var(--point-color)' : 'var(--light-gray-color)')};
     color: var(--white-color);
     align-self: flex-end;
-    padding: 4px 16px;
-    cursor:  ${(props) => (props.disabled ? '' : 'pointer')};
+    padding: 6px 20px;
+    cursor:  ${(props) => (props.visibled ? 'pointer' : '')};
   }
 `;
 
-const InputBox = styled.input`
-  width: 100%;
+const Input = styled.input`
+  width: 60%;
   border: none;
   border-bottom: 1px solid var(--gray-color);
   outline: none;
-  font-size: 18px;
-  margin-top: 10px;
+  font-size: 16px;
+  margin: 20px 10px 0 0;
+  padding: 0 0 1% 2%;
   
   &::placeholder {
     color: var(--gray-color);
@@ -102,8 +115,10 @@ export default function TradeApi() {
     };
   }, []);
 
-  const [Api, setApi] = React.useState('');
-  const [modalOpen, setModalOpen] = React.useState(false);
+  const [apiKey, setApiKey] = useState('');
+  const [secretKey, setSecretKey] = useState('');
+  const [accountNumber, setAccountNumber] = useState('');
+  const [modalOpen, setModalOpen] = useState(false);
   const navigate = useNavigate();
 
   const handleOpenModal = () => {
@@ -120,12 +135,28 @@ export default function TradeApi() {
   };
 
   const handleApiChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    // 입력 값이 변경될 때마다 Api 상태를 업데이트
-    setApi(event.target.value);
+    // 입력 값이 변경될 때마다 apiKey 상태를 업데이트
+    setApiKey(event.target.value);
+  };
+
+  const handleSecretChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    // 입력 값이 변경될 때마다 apiKey 상태를 업데이트
+    setSecretKey(event.target.value);
+  };
+
+  const handleAccountChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = event.target.value.replace(/[^0-9]/g, '');
+    setAccountNumber(newValue);
   };
 
   const handleButtonClick = () => {
-    if (Api !== '') {
+    if (apiKey === '') {
+      alert('API Key를 입력해 주세요!');
+    } else if (secretKey === '') {
+      alert('SECRET Key를 입력해 주세요!');
+    } else if (accountNumber === '') {
+      alert('계좌 번호를 입력해 주세요!');
+    } else {
       // axios 요청
       // 200 이면 ai 트레이딩 페이지로 이동 (임시로 그냥 이동)
       navigate('/trade');
@@ -135,7 +166,7 @@ export default function TradeApi() {
   return (
     <ApiContainer>
       <Container dark={true}>
-        <div style={{ width: '530px' }}>
+        <div style={{ width: '540px' }}>
           <InnerContainer>
             <Text>API Key가 없으신가요? <DoubtsButton onClick={handleOpenModal}/></Text>
             <Text className='small'>SSAFI에서는 한국투자증권 API Key를 통해 계좌 인증을 진행하고 있으며,
@@ -147,13 +178,36 @@ export default function TradeApi() {
       </Container>
       <Container dark={false}>
         <InnerContainer>
-          <InputKey disabled={false}>API Key를 입력해주세요.</InputKey>
-          <InputBox
-            placeholder="API Key를 입력하세요"
-            value={Api}
-            onChange={handleApiChange}
-          />
-          <InputKey disabled={Api === ''} className='button' onClick={handleButtonClick}>입력하기</InputKey>
+          <InputName className='header'>자동 투자를 위한 정보를 입력해주세요.</InputName>
+          <div>
+            <InputBox>
+              <InputName className='name'>API Key :</InputName>
+              <Input
+                placeholder="API Key를 입력하세요"
+                value={apiKey}
+                onChange={handleApiChange}
+              />
+            </InputBox>
+            <InputBox>
+              <InputName className='name'>SECRET Key :</InputName>
+              <Input
+                placeholder="SECRET Key를 입력하세요"
+                value={secretKey}
+                onChange={handleSecretChange}
+              />
+            </InputBox>
+            <InputBox>
+              <InputName className='name'>계좌 번호 :</InputName>
+              <Input
+                placeholder="계좌 번호를 입력하세요"
+                value={accountNumber}
+                onChange={handleAccountChange}
+              />
+            </InputBox>
+          </div>
+          <InputName visibled={apiKey !== '' && secretKey !== '' && accountNumber !== ''} className='button' onClick={handleButtonClick}>
+            입력하기
+          </InputName>
         </InnerContainer>
       </Container>
       {modalOpen && (
