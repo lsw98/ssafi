98ac8a63910765bf1dab9e9d2a324ad1d00edf0e
gus991121 <gus991121@naver.com>
2023-10-05 00:45:32 +0900

    style: 주식 종목 css 수정

--- ssafi-fe/src/components/Tab/StockEach.tsx
+++ ssafi-fe/src/components/Tab/StockEach.tsx
@@ -1,58 +1,103 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import styled from 'styled-components';
+import './StockTabs.css';
 
 interface Stock {
   code: string;
   name: string;
+  prdy_vrss_sign: number;
+  stck_prpr: number;
 }
 
 interface StockEachProps {
   index: number;
   stock: Stock;
+  clickedStar: Record<string, boolean>;
+  toggleStar: (stock: Stock, index: number) => void;
+  onStockClick: (stockCode: string) => void;
 }
 
 const Container = styled.div`
   width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 4px 0;
 `;
 
-export default StockEach = ({ index, stock }: StockEachProps) => {
-  // 에러 방지
+const LeftContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+`;
+
+const Name = styled.div<{ width?: number }>`
+  width: ${(props) => `${props.width}px`};
+  font-size: 14px;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+  cursor: pointer;
+`;
+
+const Price = styled.div<{ color: string }>`
+  font-size: 15px;
+  text-align: end;
+  color: ${(props) => props.color};
+  margin-left: 5px;
+`;
+
+const StockEach = ({ index, stock, clickedStar, toggleStar, onStockClick }: StockEachProps) => {
+  const nameRef = useRef<HTMLDivElement | null>(null);
+  const priceRef = useRef<HTMLDivElement | null>(null);
+  const [namewidth, setNameWidth] = useState(0);
+  const [pricewidth, setPriceWidth] = useState(0);
+
+  useEffect(() => {
+    if (priceRef.current) {
+      const price = priceRef.current.offsetWidth;
+      setPriceWidth(price);
+      setNameWidth(150 - price);
+    }
+  }, [stock.stck_prpr]);
 
   return (
     <Container>
-      <svg
-        className={`star ${clickedStar[index] ? 'filled' : ''}`}
-        onClick={() => toggleStar(stock, index)}
-        xmlns="http://www.w3.org/2000/svg"
-        width="20"
-        height="19"
-        viewBox="0 0 20 19"
-        fill="none"
-      >
-        <path
-          d="M10 2.60396L11.7217 6.74345L11.9563 7.30742L12.5651 7.35623L17.0341 7.7145L13.6292 10.6311L13.1653 11.0285L13.307 11.6226L14.3473 15.9835L10.5213 13.6466L10 13.3282L9.47875 13.6466L5.65272 15.9835L6.69296 11.6226L6.83468 11.0285L6.3708 10.6311L2.96595 7.7145L7.43488 7.35623L8.04372 7.30742L8.27829 6.74345L10 2.60396Z"
-          stroke="#EFDA4C"
-          stroke-width="2"
-        />
-      </svg>
-      <div style={{ marginRight: '10px' }}>
-        <span>{stock.name}</span> {/* 종목명 */}
-      </div>
-      <div>
-        <Tooltip
-          color={
-            Number(stock.prdy_vrss_sign) === 5
-              ? 'var(--lower-color)'
-              : Number(stock.prdy_vrss_sign) === 2
-              ? 'var(--upper-color)'
-              : 'black'
-          }
+      <LeftContainer>
+        <svg
+          className={`star ${clickedStar[index] ? 'filled' : ''}`}
+          onClick={() => toggleStar(stock, index)}
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="19"
+          viewBox="0 0 20 19"
+          fill="none"
         >
-          {Number.isNaN(Number(stock.stck_prpr))
-            ? 'Loading...'
-            : Number(stock.stck_prpr).toLocaleString()}
-        </Tooltip>
-      </div>
+          <path
+            d="M10 2.60396L11.7217 6.74345L11.9563 7.30742L12.5651 7.35623L17.0341 7.7145L13.6292 10.6311L13.1653 11.0285L13.307 11.6226L14.3473 15.9835L10.5213 13.6466L10 13.3282L9.47875 13.6466L5.65272 15.9835L6.69296 11.6226L6.83468 11.0285L6.3708 10.6311L2.96595 7.7145L7.43488 7.35623L8.04372 7.30742L8.27829 6.74345L10 2.60396Z"
+            stroke="var(--middle-color)"
+            stroke-width="2"
+          />
+        </svg>
+        <Name ref={nameRef} width={namewidth} onClick={() => { onStockClick(stock.code); }}>
+          {stock.name}
+        </Name>
+      </LeftContainer>
+      <Price ref={priceRef}
+        color={
+          Number(stock.prdy_vrss_sign) === 5
+            ? 'var(--lower-color)'
+            : Number(stock.prdy_vrss_sign) === 2
+            ? 'var(--upper-color)'
+            : 'var(--black-color)'
+        }
+      >
+        {Number.isNaN(Number(stock.stck_prpr))
+          ? 'Loading...'
+          : Number(stock.stck_prpr).toLocaleString()}
+      </Price>
     </Container>
   );
 };
+
+export default StockEach;
