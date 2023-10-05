8706f152bbb5c6f4e9edbb3b1265650aacb86792
민동후 <mdh9701@naver.com>
2023-10-05 13:31:16 +0900

    feat: merge 전 진행상황 저장

--- ssafi-fe/src/mbti.tsx
+++ ssafi-fe/src/mbti.tsx
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 import Survey from './components/Survey';
 import Result from './components/Result';
 
+import instance from './api/apiControlller';
+
 const MbtiContainer = styled.div`
 display: flex;
 flex-direction: column;
@@ -12,6 +14,15 @@ padding: 0px 30px;
 
 export default function Mbti() {
   const [surveyDone, setSurveyDone] = React.useState<boolean>(true);
+
+  React.useEffect(() => {
+    const fetchMbtiPoint = async () => {
+      const data = await instance.get('/member/mbti');
+      console.log(data);
+    };
+    fetchMbtiPoint();
+  }, []);
+
   return (
     <MbtiContainer>
       {surveyDone
