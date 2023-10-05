c4727b6e67210a0ef364e2f04bab6f14a2d5dbbd
gofoxfox <gofoxfox@naver.com>
2023-10-05 12:36:00 +0900

    feat : 뉴스 목록 조회 시 최근 작성 순으로 반환

--- ssafi-be/ssafi/src/main/java/com/run/ssafi/news/repository/NewsRepository.java
+++ ssafi-be/ssafi/src/main/java/com/run/ssafi/news/repository/NewsRepository.java
@@ -1,9 +1,7 @@
 package com.run.ssafi.news.repository;
 
 import com.run.ssafi.domain.News;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface NewsRepository extends JpaRepository<News, Long> {
-    List<News> findAllByNewsCategory(String category);
+public interface NewsRepository extends JpaRepository<News, Long>, NewsCustomRepository {
 }
